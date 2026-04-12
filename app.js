f0d11afe8dd1763456020cac1b357385be364b4f
vivek123-ops <vivekshrivastav325@gmail.com>
2026-04-12 11:47:03 +0530

    fix error ejs create home page

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express=require('express');
 
 
 const app=express();
+app.set("view engine", "ejs");
+
+
+app.get('/',(req,res)=>{
+    console.log('home page');
+    res.render("home");
+})
 
 
 app.listen(3000,()=>{
